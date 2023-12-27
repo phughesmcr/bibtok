62303916c5efef96ebf1db8331d0b79612428352
Peter Hughes <12073314+phughesmcr@users.noreply.github.com>
2023-12-27 18:13:56 +0000

    go back to logging

--- routes/index.tsx
+++ routes/index.tsx
@@ -20,7 +20,7 @@ export const handler: Handlers<ApiResponse> = {
 
     try {
       const data = await fetchWithParams(url.origin, params);
-      if (!data.ok) throw new Error("Bad response");
+      // if (!data.ok) throw new Error("Bad response");
       const json = await data.json() as ApiResponse;
       return ctx.render(json);
     } catch (err) {
