6fe39d7518dafaf5ef4ab44c157e328ca9cae127
Peter Hughes <12073314+phughesmcr@users.noreply.github.com>
2023-12-28 22:41:08 +0000

    add catechism

--- routes/catechism/index.tsx
+++ routes/catechism/index.tsx
@@ -1,12 +1,21 @@
 import AppContainer from "../../components/AppContainer.tsx";
+import Catechism from "../../db/catechism.json" assert { type: "json" };
 import NavBar from "../../islands/NavBar.tsx";
 
 export default function CatechismHome() {
   return (
     <AppContainer>
-      <main role="main" className="min-w-0 min-h-0 w-full h-full">
-        <h1>Catechism</h1>
-        <p>Coming soon</p>
+      <main role="main" class="min-w-0 min-h-0 w-full h-full">
+        <div role="feed" class="w-full h-full overflow-y-auto hide-scrollbars touch-pan-y snap-y snap-mandatory p-2">
+          {Catechism.map((c, index) => {
+            return (
+              <article key={index} class="ui catechism w-full h-full snap-start snap-always">
+                <h1 class="w-full text-left">{c[0]}</h1>
+                <p class="w-full text-left">{c[1]}</p>
+              </article>
+            );
+          })}
+        </div>
       </main>
       <nav role="navigation" className="min-w-0 min-h-0 w-full h-full">
         <NavBar />
