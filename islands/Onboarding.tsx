50b9070010ae36a04409a478569f517ba3d80331
Peter Hughes <12073314+phughesmcr@users.noreply.github.com>
2023-12-30 17:37:51 +0000

    onboarding improvements

--- islands/Onboarding.tsx
+++ islands/Onboarding.tsx
@@ -1,25 +1,20 @@
 import { onboarded } from "@lib/state.ts";
 import IconArrowBigUpLinesFilled from "icons/arrow-big-up-lines-filled.tsx";
-import type { JSX } from "preact";
-import { useCallback } from "preact/hooks";
+import { useCallback, useRef } from "preact/hooks";
 
 export default function Onboarding() {
-  const toggleOnboarded = useCallback(
-    (
-      e:
-        | JSX.TargetedPointerEvent<HTMLDivElement>
-        | JSX.TargetedTouchEvent<HTMLDivElement>
-        | JSX.TargetedMouseEvent<HTMLDivElement>,
-    ) => {
-      onboarded.value = true;
-      localStorage.setItem("onboarded", "true");
-      e.currentTarget?.remove();
-    },
-    [onboarded],
-  );
+  const dialogRef = useRef<HTMLDivElement>(null);
+
+  const toggleOnboarded = useCallback(() => {
+    onboarded.value = true;
+    localStorage.setItem("onboarded", "true");
+    dialogRef.current?.remove();
+  }, [onboarded]);
 
   return (
     <div
+      tabIndex={0}
+      ref={dialogRef}
       hidden={onboarded.value}
       role="dialog"
       aria-label="Scroll up to get started!"
@@ -27,9 +22,18 @@ export default function Onboarding() {
       style={`display: ${onboarded.value ? "none" : "flex"};`}
       class="pointer-events-auto z-50 absolute top-0 left-0 flex-col items-center justify-center w-full h-full bg-zinc-100 text-zinc-700 opacity-70 touch-manipulation"
       onTouchStart={toggleOnboarded}
-      onPointerMove={toggleOnboarded}
+      onPointerDown={toggleOnboarded}
       onMouseDown={toggleOnboarded}
     >
+      <button
+        type="button"
+        aria-label="close dialog"
+        class="absolute top-0 right-0 text-3xl m-4"
+        tabIndex={0}
+        onClick={toggleOnboarded}
+      >
+        &times;
+      </button>
       <IconArrowBigUpLinesFilled class="w-28 h-28 animate-up" />
       <p class="font-bold text-5xl">Scroll Up</p>
     </div>
