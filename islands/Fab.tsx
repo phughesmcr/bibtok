b3e0a25080e684d823bd03859830b362e0610bd0
Peter Hughes <12073314+phughesmcr@users.noreply.github.com>
2024-01-07 16:32:33 +0000

    fixes for ACNY

--- islands/Fab.tsx
+++ islands/Fab.tsx
@@ -1,9 +1,11 @@
 import { $fabUrl, $showFab } from "@lib/state.ts";
 import { effect } from "@preact/signals";
 import IconLocation from "icons/location.tsx";
-import { useCallback } from "preact/hooks";
+import { useCallback, useRef } from "preact/hooks";
 
 export default function Fab() {
+  const fabRef = useRef<HTMLDivElement>(null);
+
   const findAChurchNearYou = useCallback(() => {
     const successCallback: PositionCallback = (position: GeolocationPosition) => {
       const { latitude, longitude } = position.coords;
@@ -19,15 +21,37 @@ export default function Fab() {
   }, []);
 
   effect(() => {
+    if (!$showFab.value) {
+      fabRef.current?.style.setProperty("display", "none", "important");
+      fabRef.current?.style.setProperty("visibility", "hidden", "important");
+    } else {
+      fabRef.current?.style.setProperty("display", "flex", "important");
+      fabRef.current?.style.setProperty("visibility", null, "important");
+    }
+  });
+
+  const hideFab = useCallback(() => {
+    $showFab.value = false;
+  }, []);
+
+  effect(() => {
     findAChurchNearYou();
   });
 
   return (
-    <>
+    <div ref={fabRef}>
       <div
-        hidden={!$showFab}
-        aria-hidden={!$showFab}
-        className="absolute top-32 left-[-10px] w-12 h-12 rounded-full bg-amber-200 shadow-xl z-40 cursor-pointer border-2 border-solid border-yellow-50 opacity-90"
+        hidden={!$showFab.value}
+        aria-hidden={!$showFab.value}
+        className="absolute top-32 left-8 w-4 h-4 rounded-full bg-zinc-900 text-white shadow-md z-40 cursor-pointer opacity-90 flex justify-center items-center"
+        onClick={hideFab}
+      >
+        &times;
+      </div>
+      <div
+        hidden={!$showFab.value}
+        aria-hidden={!$showFab.value}
+        className="absolute top-32 left-[-10px] w-12 h-12 rounded-full bg-amber-200 shadow-xl z-30 cursor-pointer border-2 border-solid border-yellow-50 opacity-90"
         aria-label="A floating button which links to a church near you"
         title="Find a church near you"
       >
@@ -41,6 +65,6 @@ export default function Fab() {
           <span className="sr-only">Find a church near you</span>
         </a>
       </div>
-    </>
+    </div>
   );
 }
--- islands/Toolbar.tsx
+++ islands/Toolbar.tsx
@@ -8,7 +8,7 @@ export default function Toolbar() {
       role="toolbar"
       aria-label="Contents"
       aria-orientation="horizontal"
-      className="fixed top-0 left-0 z-40 w-full max-w-lvw h-13 mt-2 px-2 flex flex-row justify-center items-center flex-nowrap gap-1 bg-transparent text-zinc-400"
+      className="fixed top-0 left-0 z-20 w-full max-w-lvw h-13 mt-2 px-2 flex flex-row justify-center items-center flex-nowrap gap-1 bg-transparent text-zinc-400"
     >
       <TranslationSelect />
       <BookSelect />
